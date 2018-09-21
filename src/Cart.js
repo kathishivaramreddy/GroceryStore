61e9622244bff1e1df98b87fa83bf5560805c0b6
kathishivaramreddy <kathishivaram@gmail.com>
2018-09-21 16:55:30 +0530

    Total Price issue solve now price insync with quanity

--- src/Cart.js
+++ src/Cart.js
@@ -5,10 +5,9 @@ import {Link} from 'react-router-dom';
 export class Cart extends React.Component{
   render(){
       let amount=0;
-      console.log('inside cart',this.props)
       let listItems = this.props.data.map( (value) => <div className="cart"> {value.name} <hr/>{value.currency} {value.price} <br/>{value.quantity} </div>)
-      console.log('listItems',listItems)
-      let total = this.props.data.map( (value) => amount=amount+value.price )
+
+      let total = this.props.data.map( (value) => amount=amount+value.price * value.quantity )
       return(
       <div >
 
