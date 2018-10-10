a88495e1769489d68708b36bf231229b342b17f0
kathishivaramreddy <kathishivaram@gmail.com>
2018-10-10 19:19:13 +0530

    [Shiva/Sumi] Adding total price ,clear cart button to cart

--- src/Cart.js
+++ src/Cart.js
@@ -8,38 +8,35 @@ export class Cart extends React.Component {
 
   render() {
 
-    const emptyCart =  <h3>No Products Added</h3>;
+    const emptyCart =  <p>Your Cart is empty.Start shopping now</p>;
 
 
-    const listItems = this.props.data.map( (value) => <li className="cart" key={value.name}>
-      <img className ="cartImage" src={value.image} alt='image' /> <br/> {value.name} <br/> Cost:-{value.price} {value.currency} <br/>Quantity :- {value.quantity} </li>);
+    const listItems = this.props.data.map( (value) =>
 
-    const total = sum(this.props.data.map((product) => product.price * product.quantity));
-    return (
+    <li className="cart" key={value.name}>
+      <img className ="cartImage" src={value.image} alt='image' /> <br/> {value.name} <br/> Cost:-{value.price} {value.currency} <br/>Quantity :- {value.quantity}
+    </li>);
+
+    const total = <div> <b> Total Charges:-</b>{sum(this.props.data.map((product) =>
+    product.price * product.quantity ))} INR </div>;
+
+    const clearCart =<div> <h4>Cart</h4>
+      <button className="clearCart" onClick={this.props.clearCart}>Clear Cart</button> </div>
+
+      return (
       <div >
-        {/* <h1>Cart</h1>
-        <button className="clearCart" align="right" onClick={this.props.clearCart}>Clear Cart</button> */}
 
         <div class="dropdown">
           <button class="dropbtn" ><img className="cartIcon" src={require('./images/cartimage.jpg')}/>Cart </button>
           <div class="dropdown-content">
-
+            {isEmpty(this.props.data) ? '' : clearCart }
             <ul className="cart">
               {isEmpty(this.props.data) ? emptyCart : listItems }
-                </ul>
-
+            </ul>
+            {isEmpty(this.props.data) ? '' :  total}
+            {isEmpty(this.props.data) ? '' :<Link className="button" to='checkout'>Checkout</Link>}
           </div>
         </div>
-
-
-
-        {/* <div className="cart">
-          <div className="totalbox">
-            Total Amount To Be Paid :-  {total} INR
-          </div>
-          </div>
-
-        <Link className="button" to='checkout'>Pay</Link> */}
 
       </div>
     );
--- src/Cart.test.js
+++ src/Cart.test.js
@@ -12,27 +12,24 @@ describe('Cart tests', () => {
   const callback = jest.fn();
   it('should have heading h1 with text Cart and paragraph', () => {
     const wrapper = shallow(<Cart data={passedData}/>);
-    expect(wrapper.exists('h1')).toEqual(true);
-    expect(wrapper.find('h1').text()).toEqual('Cart');
-    expect(wrapper.find('p').text()).toEqual('Following products have been added to cart ')
-
+    expect(wrapper.exists('h4')).toEqual(true);
+    expect(wrapper.find('h4').text()).toEqual('Cart');
   });
 
   it('should display products in cart when products are passed',() => {
 
     const wrapper = shallow(<Cart data={passedData}/>);
-    expect(wrapper.find('div').at(2).text()).toEqual("     apple  Cost:-150 INR Quantity :-  ");
+    expect(wrapper.exists('li')).toEqual(true);
 
   })
 
   it('should display message when no products are added to cart',() => {
     const wrapper = shallow(<Cart data={[]}/>);
-    expect(wrapper.find('h3').text()).toEqual('No Products Added')
+    expect(wrapper.find('p').text()).toEqual('Your Cart is empty.Start shopping now')
   })
 
   it('should have link to checkout ',()=> {
     const wrapper = shallow(<Cart data={passedData}/>);
-    expect(wrapper.exists('h1')).toEqual(true);
     expect(wrapper.find('Link').props().to).toEqual('checkout')
   })
 
